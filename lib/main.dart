0ce3dc37014d4ed2ed12f840578f3fb8d095c3de
Robert Wood Jr <rdrwood@gmail.com>
2020-02-02 14:44:54 -0800

    playing with padding and margins in a container

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
-      )
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.lightGreen,
+          body: SafeArea(
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+              margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 50.0),
+              padding: EdgeInsets.all(20.0),
+              color: Colors.red,
+              child: Text('Hello'),
+            ),
+          ),
+        )
+    );
+  }
 }
