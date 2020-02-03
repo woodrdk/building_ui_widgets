7613673c5a0af91a5238bc1c61db6dc016281f32
Robert Wood Jr <rdrwood@gmail.com>
2020-02-02 16:00:00 -0800

    assembled rows and columns to form contact info

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,64 @@ class MyApp extends StatelessWidget {
                   "Robert Wood Jr",
                   style: TextStyle(
                     fontSize: 40.0,
+                    fontFamily: 'Pacifico',
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Developer",
+                  style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.phone_android,
+                        size: 50.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '253.223.3253',
+                        style: TextStyle (
+                          color: Colors.black,
+                          fontFamily: 'Pacifico',
+                          fontSize: 25.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.email,
+                        size: 50.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'rdrwood@gmail.com',
+                        style: TextStyle (
+                          color: Colors.black,
+                          fontFamily: 'Pacifico',
+                          fontSize: 25.0,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
